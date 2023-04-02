1f1268d0ce1a8cf271f1d1114961f2da7fb6d9f4
dvidcnev <rezzyze@gmail.com>
2023-04-02 03:17:27 +0200

    Commit

--- Form/form.js
+++ Form/form.js
@@ -1,14 +1,7 @@
-function handleData()
-{
-    var form_data = new FormData(document.querySelector("form"));
-    
-    if(!form_data.has("langs[]"))
-    {
-        document.getElementById("chk_option_error").style.visibility = "visible";
+function changeCheckboxValue(checkbox) {
+    if (checkbox.checked) {
+        checkbox.value = 'checked';
+    } else {
+        checkbox.value = 'unchecked';
     }
-    else
-    {
-        document.getElementById("chk_option_error").style.visibility = "hidden";
-    }
-    return false;
 }
